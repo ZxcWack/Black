859a9c1abcea2d9090a7d42ce96c0381d38875b2
ZxcWack <140092988+ZxcWack@users.noreply.github.com>
2024-06-23 15:58:27 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const sideMenu = document.getElementById("side-menu");
+    if (sideMenu.style.width === "250px") {
+        sideMenu.style.width = "0";
+    } else {
+        sideMenu.style.width = "250px";
+    }
+}
+
+function redirectToTelegram() {
+    window.location.href = "https://t.me/BlackHole_bookbot";
+}
